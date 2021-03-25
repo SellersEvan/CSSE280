2cdad873016d5bb320c0bc78066b5730a38cb272
Evan Sellers <sellersew@gmail.com>
2021-03-24 21:14:02 -0400

    added resume page

--- Person Portfolio/public/script/_root.js
+++ Person Portfolio/public/script/_root.js
@@ -1,1 +1,13 @@
-AOS.init();
+AOS.init({
+    easing: 'ease-out-cubic',
+    once: true,
+    offset: 50
+});
+
+window.addEventListener( "scroll", ( event ) => {
+    if ( this.scrollY >= 100 ) {
+        document.querySelector( ".navigation" ).classList.add( "shadow" );
+    } else {
+        document.querySelector( ".navigation" ).classList.remove( "shadow" );
+    }
+});
